550ee19e9d8417092789c70693d0ec1889570a62
Taylor Williams <taylor@Taylors-MacBook-Pro-2.local>
2020-11-17 20:12:03 -0700

    Working on pages

--- src/about/About.js
+++ src/about/About.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import Iframe from 'react-iframe'
+import strings from './about-strings.json'
 
 import './About.css'
 
 export default function About() {
   return (
     <div className='about-container'>
-      
-      <Iframe 
-        src="resume.pdf"
-        width="600px"
-        height="600px"/>
+      <div>
+        <h1>Education</h1>
+        <p>{strings.education}</p>
+      </div>
+      <div>
+        <h1>Experience</h1>
+        <p>{strings.experience}</p>
+      </div>
+      <div style={{width: '600px'}}>
+        <Iframe 
+          src="resume.pdf"
+          width="600px"
+          height="700px"/>
+      </div>
     </div>
   )
 }
